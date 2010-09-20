b7ed0dd31ae5751f04f094e488f6728a880d0f44
daruuin <devnull@localhost>
2010-09-20 21:55:33 +0800

    Implement a variable-sized sliding window for stats computation

--- ioswitch/ioswitch.c
+++ ioswitch/ioswitch.c
@@ -14,9 +14,80 @@
 
 #define DEV_PATH	"/dev/sda"
 #define DECISION_PT	0.5
+#define MAX_SAMPLES	5
+
+
+struct raw_stats {
+	unsigned long rreq;
+	unsigned long wreq;
+	unsigned long long rsec;
+	unsigned long long wsec;
+};
+
+
+struct avg_stats {
+	unsigned long rreq_size;
+	unsigned long wreq_size;
+	unsigned long req_size;
+};
 
 
 static struct task_struct *monitor = NULL;
+/* circular buffer for stat samples */
+static struct raw_stats samples[MAX_SAMPLES];
+static struct raw_stats *cur, *old;
+static short index = 0;
+
+
+void init_buffer(void)
+{
+	short i;
+
+	for (i = 0; i < MAX_SAMPLES; i++) {
+		samples[i].rreq = 0;
+		samples[i].wreq = 0;
+		samples[i].rsec = 0;
+		samples[i].wsec = 0;
+	}
+}
+
+
+void read_stats(struct hd_struct *p)
+{
+	cur = &samples[index];
+	cur->rreq = part_stat_read(p, ios[READ]);
+	cur->rsec = (unsigned long long)part_stat_read(p, sectors[READ]);
+	cur->wreq = part_stat_read(p, ios[WRITE]);
+	cur->wsec = (unsigned long long)part_stat_read(p, sectors[WRITE]);
+	/* get next index */
+	if (index < MAX_SAMPLES - 1)
+		index++;
+	else
+		index = 0;
+	/* and reference to the oldest reading */
+	old = &samples[index];
+}
+
+
+void compute_avg_stats(struct avg_stats *s, struct raw_stats *ref)
+{
+	if (ref) {
+		if (cur->rreq != ref->rreq)
+			s->rreq_size = (cur->rsec - ref->rsec) / (cur->rreq - ref->rreq);
+		else
+			s->rreq_size = 0;
+
+		if (cur->wreq != ref->wreq)
+			s->wreq_size = (cur->wsec - ref->wsec) / (cur->wreq - ref->wreq);
+		else
+			s->wreq_size = 0;
+	} else {
+		s->rreq_size = cur->rsec / cur->rreq;
+		s->wreq_size = cur->wsec / cur->wreq;
+	}
+
+	s->req_size = (s->rreq_size + s->wreq_size) / 2;
+}
 
 
 int threadfn(void *data)
@@ -26,39 +97,17 @@ int threadfn(void *data)
 #ifdef ELV_SWITCH
 	struct request_queue *q = bdev_get_queue(bdev);
 #endif
-
+	struct avg_stats all, window;
 	unsigned long peak = 0;
-	unsigned long ave, spr, spw, s_ave = 0, s_spr = 0, s_spw = 0;
-	unsigned long c_ior, c_iow, p_ior = 0, p_iow = 0;
-	unsigned long long c_sr, c_sw, p_sr = 0, p_sw = 0;
 
 	while (!kthread_should_stop()) {
-		c_ior = part_stat_read(p, ios[READ]);
-		c_sr = (unsigned long long)part_stat_read(p, sectors[READ]);
-		c_iow = part_stat_read(p, ios[WRITE]);
-		c_sw = (unsigned long long)part_stat_read(p, sectors[WRITE]);
-		spr = c_sr / c_ior;
-		spw = c_sw / c_iow;
-		ave = (spr + spw) / 2;
-		if (c_ior != p_ior) {
-			s_spr = (c_sr - p_sr) / (c_ior - p_ior);
-			p_ior = c_ior;
-			p_sr = c_sr;
-		} else {
-			s_spr = 0;
-		}
-		if (c_iow != p_iow) {
-			s_spw = (c_sw - p_sw) / (c_iow - p_iow);
-			p_iow = c_iow;
-			p_sw = c_sw;
-		} else {
-			s_spw = 0;
-		}
-		s_ave = (s_spr + s_spw) / 2;
-		if (s_ave > peak)
-			peak = s_ave;
+		read_stats(p);
+		compute_avg_stats(&all, NULL);
+		compute_avg_stats(&window, old);
+		if (window.req_size > peak)
+			peak = window.req_size;
 #ifdef ELV_SWITCH
-		if ((float)s_ave / peak > DECISION_PT) {
+		if ((float)window.req_size / peak > DECISION_PT) {
 			if (elv_switch(q, "anticipatory") > 0)
 				printk(KERN_INFO "elevator: switch to anticipatory\n");
 		} else {
@@ -67,8 +116,9 @@ int threadfn(void *data)
 		}
 #endif
 		printk(KERN_INFO "s/r = %lu, s/w = %lu, ave = %lu; "
-			"60s: s/r = %lu, s/w = %lu, ave = %lu, peak = %lu\n",
-			spr, spw, ave, s_spr, s_spw, s_ave, peak);
+			"5m: s/r = %lu, s/w = %lu, ave = %lu, peak = %lu\n",
+			all.rreq_size, all.wreq_size, all.req_size,
+			window.rreq_size, window.wreq_size, window.req_size, peak);
 		msleep_interruptible(60000);
 	}
 
@@ -78,6 +128,7 @@ int threadfn(void *data)
 
 int init_module(void)
 {
+	init_buffer();
 	monitor = kthread_run(threadfn, NULL, "monitor");
 	printk(KERN_INFO "ioswitch loaded\n");
 	/*
