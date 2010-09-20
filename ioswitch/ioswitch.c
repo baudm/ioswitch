029f984effe5d5096befe51a6041bfa52786968a
daruuin <devnull@localhost>
2010-09-20 13:04:42 +0800

    Initial implementation of a moving average stat

--- ioswitch/ioswitch.c
+++ ioswitch/ioswitch.c
@@ -13,7 +13,7 @@
 
 
 #define DEV_PATH	"/dev/sda"
-#define BOUNDARY	30
+#define BOUNDARY	50
 
 
 static struct task_struct *monitor = NULL;
@@ -27,20 +27,31 @@ int threadfn(void *data)
 	struct request_queue *q = bdev_get_queue(bdev);
 #endif
 
-	unsigned long ave, spr, spw;
-	unsigned long ior, iow;
-	unsigned long long sr, sw;
+	unsigned long ave, spr, spw, s_ave = 0, s_spr = 0, s_spw = 0;
+	unsigned long c_ior, c_iow, p_ior = 0, p_iow = 0;
+	unsigned long long c_sr, c_sw, p_sr = 0, p_sw = 0;
 
 	while (!kthread_should_stop()) {
-		ior = part_stat_read(p, ios[READ]);
-		sr = (unsigned long long)part_stat_read(p, sectors[READ]);
-		iow = part_stat_read(p, ios[WRITE]);
-		sw = (unsigned long long)part_stat_read(p, sectors[WRITE]);
-		spr = sr / ior;
-		spw = sw / iow;
+		c_ior = part_stat_read(p, ios[READ]);
+		c_sr = (unsigned long long)part_stat_read(p, sectors[READ]);
+		c_iow = part_stat_read(p, ios[WRITE]);
+		c_sw = (unsigned long long)part_stat_read(p, sectors[WRITE]);
+		spr = c_sr / c_ior;
+		spw = c_sw / c_iow;
 		ave = (spr + spw) / 2;
+		if (c_ior != p_ior) {
+			s_spr = (c_sr - p_sr) / (c_ior - p_ior);
+			p_ior = c_ior;
+			p_sr = c_sr;
+		}
+		if (c_iow != p_iow) {
+			s_spw = (c_sw - p_sw) / (c_iow - p_iow);
+			p_iow = c_iow;
+			p_sw = c_sw;
+		}
+		s_ave = (s_spr + s_spw) / 2;
 #ifdef ELV_SWITCH
-		if (ave > BOUNDARY) {
+		if (s_ave > BOUNDARY) {
 			if (elv_switch(q, "anticipatory") > 0)
 				printk(KERN_INFO "elevator: switch to anticipatory\n");
 		} else {
@@ -48,8 +59,10 @@ int threadfn(void *data)
 				printk(KERN_INFO "elevator: switch to cfq\n");
 		}
 #endif
-		printk(KERN_INFO "sectors/read = %lu, sectors/write = %lu, ave = %lu\n", spr, spw, ave);
-		msleep_interruptible(10000);
+		//printk(KERN_INFO "s/r = %lu, s/w = %lu, ave = %lu; "
+			"60s: s/r = %lu, s/w = %lu, ave = %lu\n",
+			spr, spw, ave, s_spr, s_spw, s_ave);
+		msleep_interruptible(60000);
 	}
 
 	return 0;
