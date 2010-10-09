63dc3dd39fa6929a120feda7650f54f6c4a90b72
Jelo <devnull@localhost>
2010-10-09 08:07:53 +0800

    Able to reboot.

--- test.sh
+++ test.sh
@@ -2,13 +2,19 @@
 #
 #benchmark schedulers
 #
+
 if [ $EUID -ne 0 ]; then
 	echo "run as sudo"
 	exit 1
 fi
 
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 -ioswitch=<on/off> <workload>"
+	exit 1
+fi
+
 dir="$(cd "$(dirname "$0")" && pwd)"
-temp=($dir/benchmarks/backup/*.fio)
+fio_file=($dir/benchmarks/backup/"$2".fio)
 
 check=$(lsmod | grep throughput)
 if [ ${#check} -ne 0 ]; then
@@ -20,23 +26,30 @@ if [ ${#check} -ne 0 ]; then
 	rmmod ioswitch
 fi
 
-insmod $dir/ioswitch/ioswitch.ko
-sleep 20
+if [ ${1:10} == "on" ]; then
+	insmod $dir/ioswitch/ioswitch.ko
+	sleep 20
+fi
 
 line=$(wc /var/log/messages | awk -F' ' '{print $1}')
 insmod $dir/throughput/throughput.ko
-t0=$(date +%s)
-for n in {1..5}; do
-	for fio_file in ${temp[*]}; do
-		$dir/benchmarks/run.sh $fio_file
-		t1=$(date +%s)
-		echo $(($t1-St0)) >> timestamp
-	done
-done
+$dir/benchmarks/run.sh $fio_file
 sleep 30
 rmmod throughput
-rmmod ioswitch
 
-tail -n +$(($line+1)) /var/log/messages | grep TMM > BW
+if [ ${1:10} == "on" ]; then
+	rmmod ioswitch
+fi
+
+for n in {1..5} do
+	check=$(ls | grep BW"$n")
+	if [ ${#check} -eq 0 ]; then
+		tail -n +$(($line+1)) /var/log/messages | grep TMM > $2_BW$n
+		grep BW $2_BW$n | awk -F' ' '{print $8}' | cut -c 5- > $2_read$n
+		grep BW $2_BW$n | awk -F' ' '{print $9}' | cut -c 5- > $2_write$n
+
+		reboot
+	fi
+done
 
 exit 0
